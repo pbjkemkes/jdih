0c4f3dae3fd94969aa18cbd573effac2e4b70dd8
pbjkemkes <ukpbj.kemkes@gmail.com>
2026-06-22 15:20:40 +0700

    Create masuk.js

---
+++ js/masuk.js
@@ -0,0 +1,33 @@
+import { auth } from "./firebase-config.js";
+
+import {
+sendSignInLinkToEmail
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const actionCodeSettings = {
+  url: "https://pbjkemkes.github.io/jdih/finish.html",
+  handleCodeInApp: true
+};
+
+document.getElementById("btnLogin")
+.onclick = async ()=>{
+
+  const email =
+  document.getElementById("email").value.trim();
+
+  if(!email.endsWith("@kemkes.go.id")){
+    alert("Gunakan email @kemkes.go.id");
+    return;
+  }
+
+  await sendSignInLinkToEmail(
+    auth,
+    email,
+    actionCodeSettings
+  );
+
+  localStorage.setItem("emailForSignIn",email);
+
+  alert("Link login telah dikirim.");
+};
