a6860ae630f1cef72bcf539581098c831018d2a1
pbjkemkes <ukpbj.kemkes@gmail.com>
2026-06-22 14:57:46 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -10,13 +10,21 @@ import { getFirestore } from
 "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "xxxxxxxx",
-  authDomain: "xxxxxxxx.firebaseapp.com",
-  projectId: "xxxxxxxx",
-  storageBucket: "xxxxxxxx.appspot.com",
-  messagingSenderId: "xxxxxxxx",
-  appId: "xxxxxxxx"
+
+  apiKey: "AIzaSyCJ6vhWRd7Kw7o24yus8zP22HViIcral7k",
+
+  authDomain: "jdih-biro-pbj.firebaseapp.com",
+
+  projectId: "jdih-biro-pbj",
+
+  storageBucket: "jdih-biro-pbj.firebasestorage.app",
+
+  messagingSenderId: "576809704014",
+
+  appId: "1:576809704014:web:2fed889d5c1b5ab757811f"
+
 };
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
