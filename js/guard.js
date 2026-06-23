6790964e2edfd91b858b0f2cdce729a2d9244759
pbjkemkes <ukpbj.kemkes@gmail.com>
2026-06-23 13:42:12 +0700

    Update guard.js

--- js/guard.js
+++ js/guard.js
@@ -1,24 +1,12 @@
 (async()=>{
 
 const {
-
 data:{user}
-
 }
 =
 await sb.auth.getUser();
 
 if(!user){
-
-location="masuk.html";
-
-return;
-
-}
-
-if(!user.email.endsWith("@kemkes.go.id")){
-
-await sb.auth.signOut();
 
 location="masuk.html";
 
