4f8442fb90761cdc24c2bf940eeb4f41f66afacd
pbjkemkes <ukpbj.kemkes@gmail.com>
2026-06-23 15:26:20 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,35 +1,100 @@
-import { db } from "./firebase-config.js";
+(async()=>{
 
-import {
-collection,
-getDocs,
-query,
-orderBy
+const {data,error} =
+await sb
+.from("access_log")
+.select("*")
+.order(
+"login_time",
+{
+ascending:false
+});
+
+let map={};
+
+data.forEach(r=>{
+
+if(!map[r.email]){
+
+map[r.email]={
+
+jumlah:0,
+
+terakhir:r.login_time
+
+};
+
 }
-from
-"https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-const q=query(
-collection(db,"access_log"),
-orderBy("waktu","desc")
-);
+map[r.email].jumlah++;
 
-const snap=await getDocs(q);
+});
+
+document.getElementById(
+"totalUser"
+).innerHTML=
+Object.keys(map).length;
+
+document.getElementById(
+"totalLogin"
+).innerHTML=
+data.length;
+
+const hariIni=
+new Date()
+.toISOString()
+.substring(0,10);
+
+document.getElementById(
+"loginHariIni"
+).innerHTML=
+data.filter(
+x=>x.login_date==hariIni
+).length;
 
 let html="";
 
-snap.forEach(doc=>{
-
-const d=doc.data();
+Object.entries(map)
+.forEach(([email,v])=>{
 
 html+=`
+
 <tr>
-<td>${d.nama}</td>
-<td>${d.email}</td>
-<td>${d.waktu?.toDate()}</td>
+
+<td>${email}</td>
+
+<td>
+${new Date(
+v.terakhir
+).toLocaleString()}
+</td>
+
+<td>
+${v.jumlah}
+</td>
+
 </tr>
+
 `;
 
 });
 
-tb.innerHTML=html;
+document
+.querySelector("tbody")
+.innerHTML=html;
+
+excel.onclick=()=>{
+
+const wb=
+XLSX.utils.table_to_book(
+tbl
+);
+
+XLSX.writeFile(
+wb,
+"log-user.xlsx"
+);
+
+};
+
+})();
